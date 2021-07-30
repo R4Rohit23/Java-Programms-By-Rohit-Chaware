8561b8f295308e8a60c0e1f1987c8c9d44073da7
Rohit2k23 <86614121+Rohit2k23@users.noreply.github.com>
2021-07-30 15:04:40 +0530

    30/07/2021

---
+++ src/com/company/CodeChef_Problems/Reverse_Me.java
@@ -0,0 +1,38 @@
+//-------------------------------------------------------Problem------------------------------------------------
+//You are given a list of N integers and you need to reverse it and print the reversed list in a new line.
+
+//------------------------------------------------------Solution------------------------------------------------------
+package com.company.CodeChef_Problems;
+import java.util.*;
+
+public class Reverse_Me {
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        try {
+            Scanner scan = new Scanner(System.in);
+            int n = scan.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = scan.nextInt();
+            }
+
+            //lets build new array
+            int[] b = new int[n];
+            int j = n;
+
+            for (int i = 0; i < n; i++) {
+                b[j - 1] = arr[i];
+                j--;
+            }
+            //printing reversed array
+            for (int k = 0; k < n; k++) {
+                System.out.print(b[k] + " ");
+            }
+
+
+        } catch (Exception ignored) {
+
+        }
+    }
+}
